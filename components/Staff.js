e2545aa74e87b71c28aa97264c1861fa21a6d2e5
Nasir <movlamovnasir2000@gmail.com>
2021-04-28 15:37:47 +0400

    newnew

--- components/Staff.js
+++ components/Staff.js
@@ -9,7 +9,8 @@ function Student(props) {
     return (
         <Link href={`/staff/${props.id}`}> 
             <div key={props.id} className={styles.studentCont}>
-                <div style={studentImg} className={styles.studentImgStyle + ' img'}></div>
+                <Image src={`url(https://creativespark.testjed.me/${props.image})`} alt="Picture of the author" width={500} height={500} /><Image/>
+                {/* <div style={studentImg} className={styles.studentImgStyle + ' img'}></div> */}
                 <p className={styles.name + " text"}>{props.name}</p>
                 <p className={styles.job + " text"}>{props?.type}</p>
             </div>
