921f0808a89ecdb28dfaf340885f7a84b609865c
Nasir <movlamovnasir2000@gmail.com>
2021-05-24 09:49:16 +0400

    neüneü

--- pages/about.js
+++ pages/about.js
@@ -24,17 +24,14 @@ function about() {
                 <h1 className={styles.aboutTitle + " title-b-desk pageTitle"} data-aos="fade-up">{langM === "AZ" && `Haqqımızda` || langM === "EN" && `About Us` || langM === "RU" && `О нас`}</h1>
                 <div className={styles.aboutPart1}  data-aos="fade-up">
                     <p className={styles.text + ' text'}> 
-                        {langM === "AZ" && `"Creative Spark" Birləşmiş Krallığın dəstəyilə Mərkəzi Asiya ölkələrində, Cənubi Qafqaz
-ölkələrində və Ukraynada biznes bacarıqlarını və yaradıcı iqtisadiyyatı inkişaf etdirmək
-məqsədilə universitetlərarası və institusional əməkdaşlığı beynəlxalq səviyyədə
-dəstəkləmək üçün nəzərdə tutulan beş illik təşəbbüsdür.` 
+                        {langM === "AZ" && `"Creative Spark" Birləşmiş Krallığın dəstəyilə Mərkəzi Asiya ölkələrində (Qazaxıstan, Özbəkistan, Qırğızıstan), Cənubi Qafqaz ölkələrində (Azərbaycan, Gürcüstan, Ermənistan) və Ukraynada (ümumilikdə yeddi ölkədə) biznes bacarıqlarını və yaradıcı iqtisadiyyatı inkişaf etdirmək məqsədilə universitetlərarası və institusional əməkdaşlığı beynəlxalq səviyyədə dəstəkləmək üçün nəzərdə tutulan beş illik təşəbbüsdür.` 
                             || langM === "EN" && `Creative Spark is a five year initiative to support international university and institutional partnerships to develop enterprise skills and creative economy across seven countries in Central Asia (Kazakhstan, Uzbekistan, Kyrgyzstan), South Caucasus (Azerbaijan, Armenia, Georgia) and Ukraine through UK support.  ` 
-                            || langM === "RU" && `Creative Spark - это пятилетняя инициатива по поддержке международных университетов и созданию международных партнерств с целью развить предпринимательские навыки и креативную экономику в семи странах, включая Центральную Азию (Казахстан, Узбекистан, Кыргызстан), Южный Кавказ (Азербайджан, Армению, Грузию) и Украину при поддержке Соединенного Королевства. `}
+                            || langM === "RU" && `Creative Spark – это пятилетняя программа по развитию международного образовательными учреждениями Великобритании и странами- участниками проекта в области развития навыков креативного предпринимательства в целях развития креативной экономики. В проекте участвуют: страны Средней Азии (Казахстан, Узбекистан, Кыргызстан), Южного Кавказа (Азербайджан, Армения, Грузия) и Украина. `}
                         <br/>
                         <br/>
                         {langM === "AZ" && `Bu yeddi ölkədə ali təhsil sahəsində aparılan islahatları dəstəkləmək və işsizlik səviyyəsinin aşağı salınması üçün Birləşmiş Krallığın təcrübəsindən istifadə edəcəyik.` 
                             || langM === "EN" && `In order to support higher education reform and respond to unemployment rates in these seven countries, the UK’s experience will be used to help develop enterprise education. This way over 10,000 students and young entrepreneurs were supported in the first year of the programme.   ` 
-                            || langM === "RU" && `Чтобы поддержать реформы в сфере высшего образования и содействовать в решении проблемы трудоустройства в этих семи странах, мы будем использовать опыт Соединенного Королевства для развития образования в области предпринимательства. Таким образом мы окажем поддержку 10 000 студентам и молодым предпринимателям в течение первого года программы. `}
+                            || langM === "RU" && `В целях поддержки реформ в высшем образовании и развития навыков успешного трудоустройства, программа внесет вклад в развитие навыков креативного предпринимательства в рамках образовательного процесса, используя опыт Великобритании.`}
 
                     </p>
                     <img   src="/about.png" alt="" className={styles.img}/>
@@ -44,22 +41,22 @@ dəstəkləmək üçün nəzərdə tutulan beş illik təşəbbüsdür.`
                     <p className={styles.text + ' text'}  data-aos="fade-right">
                         {langM === "AZ" && `Proqram üç əsas elementdən ibarətdir: ` 
                             || langM === "EN" && `The programme has three key elements: ` 
-                            || langM === "RU" && `Программа состоит из трех основных элементов: `}
+                            || langM === "RU" && `Программа состоит из трех ключевых элементов:`}
                     <br/>
                     <br/>
                         {langM === "AZ" && `1 Biznes təhsilini dəstəkləmək və biznes mərkəzləri yaratmaq məqsədilə İngiltərədəki universitetlər və yaradıcı müəssisələr arasında ortaqlığın inkişaf etdirilməsi.` 
                             || langM === "EN" && `1. Development of partnerships between universities and creative institutions in the UK and those in programme countries in order to support enterprise education and establish enterprise centres. ` 
-                            || langM === "RU" && `1. Развитие партнерских отношений между университетами и творческими институтами в Великобритании и странах, в которых осуществляются программы, с целью поддержки предпринимательского образования и создания центров предпринимательства. `}
+                            || langM === "RU" && `1. Развитие партнерства между университетами и креативными учреждениями в Великобритании и в странах, участвующих в программе, а также поддержка развития навыков креативного предпринимательства в рамках образовательного процесса и создание центров предпринимательства`}
                     <br/>
                     <br/>
                         {langM === "AZ" && `2 Tələbələrə və yaradıcı sahibkarlara biznes bacarıqlarının öyrədilməsi, ideyaların təqdim edilməsi, biznesin qurulması, əqli mülkiyyətin qorunması və kredit xətlərinin qorunması mövzusunda təlim paketlərinin təklif olunması.` 
                             || langM === "EN" && `2. Delivering enterprise skills training packages to students and creative entrepreneurs, ranging from pitching ideas and starting a business, to protecting intellectual property and securing credit lines. ` 
-                            || langM === "RU" && `2. Предоставление студентам и творческим предпринимателям программы обучения предпринимательским навыкам, начиная от подачи идей и открытия бизнеса до защиты интеллектуальной собственности и обеспечения кредитных линий. `}
+                            || langM === "RU" && `2.  Проведение серий тренингов по навыкам предпринимательства  для студентов и креативных предпринимателей, начиная с  презентации идеи и открытия бизнеса, заканчивая защитой  интеллектуальной собственности и получением финансирования  на реализацию проекта.`}
                     <br/>
                     <br/>
                         {langM === "AZ" && `3 İngilis dilini öyrənmə proqramının, rəqəmsal tədris materiallarının, o cümlədən, təlim platformalarının, onlayn kursların və geniş kütlə üçün açıq olan onlayn kursların (MOOC) sahibkarlar üçün ingilis dili səviyyəsində təklif edilməsi.` 
                             || langM === "EN" && `3. Delivering an English learning programme with a range of digital learning content including learning platforms, online courses and Massive Open Online Courses (MOOCs) focused on English for entrepreneurship. ` 
-                            || langM === "RU" && `3. Предоставление программы изучения английского языка с разнообразным цифровым учебным контентом, включая учебные платформы, онлайн-курсы и массовые открытые онлайн-курсы (МООК), ориентированные на английский для предпринимательства. `}
+                            || langM === "RU" && `3.  Изучение английского языка с помощью различных онлайн  ресурсов, включая образовательные платформы, онлайн курсы с  фокусом на креативное предпринимательство.`}
                     </p>
                 </div>
 
